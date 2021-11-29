80909cdc19d730a9a86abfc0892e27a97a63a143
KracoY <enderyuruk9932@gmail.com>
2021-11-29 14:33:39 +0100

    update

---
+++ javascript/tutorial1/app.js
@@ -0,0 +1,8 @@
+console.log('hello')
+console.log('helloooooo')
+console.log('test.js')
+//alert('hello javascript')
+//alert('<p>this is a pargraph</p>')
+document.write("<h1>hello js</h1>")
+document.writeln('hi everybody')
+document.getElementById("intecBrussel").innerHTML="Welcome to Intec Brussel"
---
+++ javascript/tutorial2/index.js
@@ -0,0 +1,140 @@
+var webdevnov = 1;
+var webdevnov1 = 20;
+console.log(webdevnov)
+console.log(webdevnov1)
+
+
+var smartphone = 'samsung'  //declaration of a variable
+var smartphone = 'apple'  //re- declaration with different value
+smartphone = 'android'  //update
+
+
+
+//const - let
+
+// const variable
+
+const firstname = 'ender'
+//re declaration
+//const firstname = 'demet'
+//syntaxerror: identifier 'firstname' has already been declared
+console.log(firstname)
+
+
+//update
+//firstname = 'demet'
+
+// Typeerror: asigment to constant variable.
+
+//let variable
+
+let companyname = 'intecbrussel'
+
+//re-deck
+companyname = 'microsoft'
+    //let variables can be updated
+
+    /*
+    var variable
+    can be re-declaration, update
+    
+    let variable
+    can not be re-declaration but can be updated
+    */
+
+
+    / console.log(companyname)
+
+let myMessage;
+
+myMessage = 'hello world'
+console.log(myMessage)
+
+let _companyName = 'hello' //correct usage
+let company_Name = 'Hello'
+//let company-name = 'hello' // incorrect usage
+
+var number1 = 10;
+var number2 = 2;
+// "=" equal to
+var result = number1 + number2
+result = number1 - number2
+
+var rst1 = "intec";
+var rst2 = " brussel";
+result = rst1 + rst2 //string(text)
+
+//division
+var _number1 = 9;
+var _number2 = 2;
+result = _number1 / _number2
+
+// netto salary
+var mySalary = 2000;
+var taxprecent = 20;
+var tax = 100 - taxprecent;
+var taxFinal = tax / 100;
+var result = mySalary * taxFinal;
+console.log(result)
+
+// increment operator "++"
+result = _number1
+
+// decrease operator "--"
+_number1--
+result = _number1--
+
+let x = (10 * 3) + (3 + 3);
+// console.log(x)
+
+var a = 10; // 10 = 10 + 10 =
+a = a + a // 20
+a = a * 2 // 20*2=40
+
+//power
+result = Math.pow(9,2);
+
+console.log("a result",a)
+// console.log(result)
+
+//string (text)
+var result;
+result= 10 //number
+result = true //true or false
+result= "string" + 4
+
+var nr1 = 5; // number
+var nr2 = "5"; //string (text)
+var res = nr1+nr2 //5 5
+console.log(res)
+
+//array
+const myDB = ["yuruk","sameer","khaled","fatema","balin"] //array-object
+const db= ['1','2','3',true,5,_number1]
+myDB[1]="ender"
+console.log(myDB)
+
+
+//object
+let student = {
+    name: 'ender',
+    lastname: 'Yuruk',
+    age: 22,
+    isStudent:true,
+    score:[70, 80, 90, 100]
+}
+
+//exersize 
+let exersize = {
+    name: 'ender',
+    lastname: 'Yuruk',
+    age: 22,
+    student:  true,
+    birthday: '25/06/1999',
+    location: 'puurs'
+}
+
+console.log(exersize)
+
+
+console.log(result)
