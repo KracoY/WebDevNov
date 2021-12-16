b85d616e35787afd281a0eb6a9a254a2aa78f882
KracoY <enderyuruk9932@gmail.com>
2021-12-16 11:21:09 +0100

    up

---
+++ javascript/PianoApp/app.js
@@ -0,0 +1,11 @@
+let allbutons = document.querySelectorAll('.key')
+
+allbutons.forEach(el =>{
+    console.log(el);
+    console.log(el.getAttribute('data-key'))
+    //set action
+    el.addEventListener('click',function(e){
+        console.log('click')
+        //play audio
+    })
+})
